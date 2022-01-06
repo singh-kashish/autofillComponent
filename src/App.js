0f7cb8e5b28a17590e221484cf2dd30ec2679238
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-01-06 19:01:10 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Autofill from "./components/Autofill";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex-col">
+      <div className="bg-gray-100 p-14 m-40 mx-60 border-2 border-solid border-gray-300">
+        <h1 className="text-lg mb-4 pb-1 border-b-2 text-center">
+          AutoComplete for Spenny
+        </h1>
+
+        <Autofill
+          suggestions={[
+            "Apples",
+            "Bask",
+            "Crocodilian",
+            "Death Roll",
+            "Eggs",
+            "Jaws",
+            "Reptile",
+            "Solitary",
+            "Tail",
+            "Wetlands",
+            "Searching",
+          ]}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
